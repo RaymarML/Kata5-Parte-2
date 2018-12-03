d9d9f218c7d177d44bfc1b2e17146de131566241
RaymarML <32089732+RaymarML@users.noreply.github.com>
2018-12-03 17:11:45 +0000

    Kata5 parte2 version2

--- src/kata5P2/main/Kata5.java
+++ src/kata5P2/main/Kata5.java
@@ -1,16 +1,16 @@
 
 package kata5P2.main;
 import java.io.IOException;
-import kata5P2.view.MailListReader;
+import kata5P2.view.MailListReaderBD;
 import kata5P2.model.Mail;
 import kata5P2.view.MailHistogramBuilder;
 import kata5P2.model.Histogram;
 import java.util.List;
 import kata5P2.view.HistogramDisplay;
 
-public class Kata4 {
+public class Kata5 {
     
-    static List<Mail> listMails;
+    static List<String> listMails;
     static Histogram c;
     
     public static void main(String[] args) throws IOException{
@@ -22,8 +22,8 @@ public class Kata4 {
 
     
     public static void input() throws IOException{
-        MailListReader a = new MailListReader();
-        listMails = a.read("C:\\Users\\Armando\\Desktop\\a.txt");
+        MailListReaderBD a = new MailListReaderBD();
+        listMails = a.read("jdbc:sqlite:Kata5.db", "EMAIL");
     }
     
     public static void process(){
---
+++ src/kata5P2/view/MailListReaderBD.java
@@ -0,0 +1,37 @@
+package kata5P2.view;
+
+import kata5P2.model.Mail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MailListReaderBD {
+    
+    public MailListReaderBD(){}
+    
+    public List<String> read(String url, String table) throws IOException{
+        List<String> mails = new ArrayList<>();
+        String sql = "SELECT * FROM " + table;
+        try (Connection conn = DriverManager.getConnection(url); 
+                Statement stmt = conn.createStatement(); 
+                ResultSet rs = stmt.executeQuery(sql)){
+            while (rs.next()) { 
+                mails.add(rs.getString("mail")); 
+            } 
+        } catch (SQLException e) { 
+            System.out.println(e.getMessage()); 
+        }
+        return mails;
+    }
+    
+}
